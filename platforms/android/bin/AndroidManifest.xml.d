1938980ba296d3a5cdb7a85358504845d0864f00
Anear Nahar <deathangel5553@gmail.com>
2013-08-03 23:24:38 -0400

    intial commit

---
+++ platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\user\Desktop\MealerAPP\platforms\android\bin\AndroidManifest.xml : \
+C:\Users\user\Desktop\MealerAPP\platforms\android\AndroidManifest.xml \
---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,14 @@
+C:\Users\user\Desktop\MealerAPP\platforms\android\gen\com\mealer\app\R.java \
+ : C:\Users\user\Desktop\MealerAPP\platforms\android\res\drawable\icon.png \
+C:\Users\user\Desktop\MealerAPP\platforms\android\res\drawable-hdpi\icon.png \
+C:\Users\user\Desktop\MealerAPP\platforms\android\res\drawable-hdpi\ic_launcher.png \
+C:\Users\user\Desktop\MealerAPP\platforms\android\res\drawable-ldpi\icon.png \
+C:\Users\user\Desktop\MealerAPP\platforms\android\res\drawable-ldpi\ic_launcher.png \
+C:\Users\user\Desktop\MealerAPP\platforms\android\res\drawable-mdpi\icon.png \
+C:\Users\user\Desktop\MealerAPP\platforms\android\res\drawable-mdpi\ic_launcher.png \
+C:\Users\user\Desktop\MealerAPP\platforms\android\res\drawable-xhdpi\icon.png \
+C:\Users\user\Desktop\MealerAPP\platforms\android\res\drawable-xhdpi\ic_launcher.png \
+C:\Users\user\Desktop\MealerAPP\platforms\android\res\layout\main.xml \
+C:\Users\user\Desktop\MealerAPP\platforms\android\res\values\strings.xml \
+C:\Users\user\Desktop\MealerAPP\platforms\android\res\xml\config.xml \
+C:\Users\user\Desktop\MealerAPP\platforms\android\bin\AndroidManifest.xml \

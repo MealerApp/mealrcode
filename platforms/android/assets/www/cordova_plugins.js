a9fcbe29658cb264e889b5d61d903dc8a75544b9
Sam Bauch <sbauch@gmail.com>
2013-08-15 19:05:05 -0400

    ugh

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,28 +1,28 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins\\org.apache.cordova.core.camera\\www\\CameraConstants.js",
+        "file": "plugins/org.apache.cordova.core.camera/www/CameraConstants.js",
         "id": "org.apache.cordova.core.camera.Camera",
         "clobbers": [
             "Camera"
         ]
     },
     {
-        "file": "plugins\\org.apache.cordova.core.camera\\www\\CameraPopoverOptions.js",
+        "file": "plugins/org.apache.cordova.core.camera/www/CameraPopoverOptions.js",
         "id": "org.apache.cordova.core.camera.CameraPopoverOptions",
         "clobbers": [
             "CameraPopoverOptions"
         ]
     },
     {
-        "file": "plugins\\org.apache.cordova.core.camera\\www\\Camera.js",
+        "file": "plugins/org.apache.cordova.core.camera/www/Camera.js",
         "id": "org.apache.cordova.core.camera.camera",
         "clobbers": [
             "navigator.camera"
         ]
     },
     {
-        "file": "plugins\\org.apache.cordova.core.camera\\www\\CameraPopoverHandle.js",
+        "file": "plugins/org.apache.cordova.core.camera/www/CameraPopoverHandle.js",
         "id": "org.apache.cordova.core.camera.CameraPopoverHandle",
         "clobbers": [
             "CameraPopoverHandle"
